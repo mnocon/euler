298b6a89cf17105f6d6113f14871a2e94f62d711
Marek Nocon <marek.nocon@ez.no>
2017-07-18 11:24:27 +0200

    Problem 35

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from solutions.Problem34 import Problem34
+from solutions.Problem35 import Problem35
 
-current_challenge = Problem34()
+current_challenge = Problem35()
 current_challenge.run_tests()
 solution = current_challenge.execute()
 print("Problem solution: {}".format(solution))
