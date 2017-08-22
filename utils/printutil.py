4c4504a09a1016e86f6be734b88a0c2ee225e5df
Marek Nocon <marek.nocon@ez.no>
2017-08-22 10:04:19 +0200

    Change print decorator

--- utils/printutil.py
+++ utils/printutil.py
@@ -1,5 +1,6 @@
 def console_print(function):
     def wrapper(*args, **kwargs):
+        print(args[1:], kwargs)
         value = function(*args, **kwargs)
         print(value)
         return value
